fec80cd6e28a35f7cc415026dae9862b7cf5154a
LIPiston <wps1910093789@gmail.com>
2025-03-09 22:28:01 +0800

    自修改

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ import Scel2Txt
 import os
 
 # 下载类别
-Categories = ['城市信息:167', '自然科学:1', '社会科学:76', '工程应用:96', '农林渔畜:127', '医学医药:132',
-              '电子游戏:436', '艺术设计:154', '生活百科:389', '运动休闲:367', '人文科学:31', '娱乐休闲:403']
+Categories = ['网络流行新词:4','电子游戏:436']
 # Scel保存路径
-SavePath = r"f:\Users\QQT\Documents\Temp\scel1"
+SavePath = r"./scel"
 
 # TXT保存路径
-txtSavePath = r"f:\Users\QQT\Documents\Temp\txt1"
+txtSavePath = r"./txt"
 
 # 开始链接
 startUrl = "https://pinyin.sogou.com/dict/cate/index/436"
